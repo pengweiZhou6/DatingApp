e93ba6fb2bb1fb50dd95fc02a8429351946d96bd
pengweiZhou6 <55173868+pengweiZhou6@users.noreply.github.com>
2021-04-19 21:50:51 -0700

    Day1: 5%

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,12 +5,6 @@ namespace API.Entities
         public int Id {get;set;}
         public string UserName {get;set;}
 
-        private int myVar;
-        public int MyProperty
-        {
-            get { return myVar; }
-            set { myVar = value; }
-        }
         
     }
 } 
