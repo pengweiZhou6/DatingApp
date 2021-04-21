a3334a25c8f36ad4475be16a9057cf6dbada18dc
pengweiZhou6 <55173868+pengweiZhou6@users.noreply.github.com>
2021-04-21 01:59:47 -0700

    Day2: 21%

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,14 +2,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,6 +18,7 @@ namespace API.Controllers
 
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(int id)
         {
             return await _context.Users.ToListAsync();
@@ -27,6 +27,7 @@ namespace API.Controllers
 
 
         // api/users/3
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
